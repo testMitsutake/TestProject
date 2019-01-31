4819f8f95af811ecf89650943afe2915bcdb3257
mitsutake <@DESKTOP-RRRTTR3>
2019-01-31 12:24:36 +0900

    3乗から4乗に変更

--- src/myPackage/calc/Square.java
+++ src/myPackage/calc/Square.java
@@ -2,11 +2,11 @@ package myPackage.calc;
 
 public class Square extends Calculation {
 	/**
-	 * 値を3乗します。
+	 * 値を4乗します。
 	 */
 	public void calulate() {
-		//valueを3乗する
-		this.result = this.value * this.value * this.value;
+		//valueを4乗する
+		this.result = this.value * this.value * this.value * this.value;
 		//結果を出力する
 		this.output();
 	}
