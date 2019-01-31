42937eff719f00d59ae3886e1d62cfa0226910eb
PPF-Yamamoto <44758450+PPF-Yamamoto@users.noreply.github.com>
2019-01-31 10:59:00 +0900

    2乗から3乗に変更しました

--- src/myPackage/calc/Square.java
+++ src/myPackage/calc/Square.java
@@ -2,11 +2,11 @@ package myPackage.calc;
 
 public class Square extends Calculation {
 	/**
-	 * 値を2乗します。
+	 * 値を3乗します。
 	 */
 	public void calulate() {
-		//valueを2乗する
-		this.result = this.value * this.value;
+		//valueを3乗する
+		this.result = this.value * this.value * this.value;
 		//結果を出力する
 		this.output();
 	}
